4bbe19ac17d2aa2615f81f2e170497ccb4b65521
Hamid Zade <hamidmamadzade@gmail.com>
2016-01-18 15:51:31 +0000

    Restaurant model added

--- spec/features/restaraunt_features_spec.rb
+++ spec/features/restaraunt_features_spec.rb
@@ -4,8 +4,20 @@ feature "restaraunts" do
   context "No restaraunts have been added" do
     scenario "should display a prompt to add a restaurant" do
       visit "/restaurants"
-      expect(page).to have_content "No restaraunts yet!"
+      expect(page).to have_content "No restaurants yet!"
       expect(page).to have_link "Add a restaurant"
+    end
+  end
+
+  context "restaurants have been added" do
+    before do
+      Restaurant.create(name: "Nandos")
+    end
+
+    scenario "displays restaurants" do
+      visit "/restaurants"
+      expect(page).to have_content "Nandos"
+      expect(page).not_to have_content "No restaraunts yet!"
     end
   end
 end
