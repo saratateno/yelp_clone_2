2dd15aaa479393d648405050769d2e182c823c5f
Hamid Zade <hamidmamadzade@gmail.com>
2016-01-18 15:00:30 +0000

    views added, test passes

--- spec/features/restaraunt_features_spec.rb
+++ spec/features/restaraunt_features_spec.rb
@@ -4,7 +4,7 @@ feature "restaraunts" do
   context "No restaraunts have been added" do
     scenario "should display a prompt to add a restaurant" do
       visit "/restaurants"
-      expect(page).to have_content "No restaraunts yet"
+      expect(page).to have_content "No restaraunts yet!"
       expect(page).to have_link "Add a restaurant"
     end
   end
