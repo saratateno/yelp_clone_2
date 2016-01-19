af9d3036aa5a042d7ddec2cc24bd0ec80dabb1e3
Andrew Burnie <apburnie@hotmail.co.uk>
2016-01-19 18:00:01 +0000

    intro sign-in sign-up functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   resources :restaurants do
     resources :reviews
   end
+
+  root to: "restaurants#index"
 
 end
