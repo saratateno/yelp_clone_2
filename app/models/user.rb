6438616b89052d07bb78f285351a228bd1bc3834
Andrew Burnie <apburnie@hotmail.co.uk>
2016-01-20 16:39:19 +0000

    disables reviewing if not logged in

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,17 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, :omniauth_providers => [:facebook]
 
+  has_many :reviews
+  has_many :reviewed_restaurants, through: :reviews, source: :restaurant
+
    def self.from_omniauth(auth)
      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
        user.email = auth.info.email
        user.password = Devise.friendly_token[0,20]
      end
+   end
+
+   def has_reviewed?(restaurant)
+     reviewed_restaurants.include? restaurant
    end
 end
--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -3,8 +3,9 @@ require "rails_helper"
 feature "reviewing" do
   let!(:nandos){Restaurant.create(name: "Nandos")}
 
-  scenario "allows users to leave a review" do
+  scenario "allows logged-in users to leave a review" do
     visit "/restaurants"
+    sign_up
     click_link "Review Nandos"
     fill_in "Opinion", with: "I don't even like chicken"
     select "3", from: "Rating"
@@ -13,4 +14,21 @@ feature "reviewing" do
     expect(current_path).to eq "/restaurants"
     expect(page).to have_content "I don't even like chicken"
   end
+
+  scenario "disallows non-logged-in users to leave a review" do
+    visit "/restaurants"
+    click_link "Review Nandos"
+    expect(current_path).to eq "/users/sign_in"
+  end
+
+
+
+  def sign_up
+    click_link "Sign Up"
+    fill_in "Email", with: "test@example.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+    click_button "Sign Up"
+  end
+
 end
